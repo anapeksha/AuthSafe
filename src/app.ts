751034b22e604e1646b03e144e6f1b527c2c929a
anapeksha <anapeksha.mukherjee@gmail.com>
2023-10-09 19:36:24 +0530

    middleware used

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import { constants } from "./config";
-import { userRoute } from "./routes";
+import { authRoute, userRoute } from "./routes";
 
 const port = constants.PORT;
 const apiVersion = constants.API_VERSION;
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(`/api/${apiVersion}`, userRoute);
+app.use(`/api/${apiVersion}`, authRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
