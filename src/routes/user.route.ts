5231021bd76cf6d114f678efb8ebafb1b3987790
anapeksha <anapeksha.mukherjee@gmail.com>
2023-11-06 21:49:15 +0530

    added middleware to protected routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { userController } from "../controllers";
+import { authorizedMiddleware } from "../middlewares";
 
 const router: Router = express.Router();
 
-router.get("/users", userController.getAllUsers);
-router.get("/users/:id", userController.getUserByID);
+router.get("/users", authorizedMiddleware, userController.getAllUsers);
+router.get("/users/:id", authorizedMiddleware, userController.getUserByID);
 router.post("/users/create", userController.createUser);
 
 export default router;
