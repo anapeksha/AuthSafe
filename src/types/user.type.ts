81fd0c41b3ea7bc0e3ed9af0ad82ebe3cd4e1b2e
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2023-11-08 01:31:16 +0530

    types

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,7 +1,11 @@
 interface IUser {
-  id: string;
-  email: string;
-  password: string;
+  id?: string;
+  email?: string;
+  password?: string;
+  name?: string | null;
+  sessions?: Array<string>;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 export { IUser };
