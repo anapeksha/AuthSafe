b4ac2ff5c1d3eb46a95f7c3b0102f400ba2c3760
anapeksha <anapeksha.mukherjee@gmail.com>
2023-11-17 16:13:45 +0530

    modified authorized middleware

--- src/middlewares/authorized.middleware.ts
+++ src/middlewares/authorized.middleware.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { verifyToken } from "../utils/jwt";
 
-const authorizedMiddleware = (req: Request, res: Response, next) => {
+const authorizedMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const authCookie = req.cookies.token;
-  const token = verifyToken(authCookie);
-  if (!token) {
-    res.status(403).send({ error: "Unauthorised" });
+  if (authCookie) {
+    const token = verifyToken(authCookie);
+    if (token) {
+      next(token);
+    } else {
+      res.status(401).send({ error: "Unauthorised" });
+    }
   } else {
-    next();
+    res.status(401).send({ error: "Unauthorised" });
   }
 };
 
